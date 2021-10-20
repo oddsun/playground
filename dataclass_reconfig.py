759c278d5154424fdce251c9b8260dc9664ba501
odd <1161465+oddsun@users.noreply.github.com>
2021-10-20 02:38:58 -0400

    trying default_factory with the hack

--- dataclass_reconfig.py
+++ dataclass_reconfig.py
@@ -1,10 +1,12 @@
-from dataclasses import dataclass, InitVar
+from dataclasses import dataclass, InitVar, field
+from typing import List
 
 
 @dataclass(frozen=True)
 class A:
     a: str = ''
     b: int = 0
+    lst: List[int] = field(default_factory=list)
     config: InitVar[dict] = None
 
     def __post_init__(self, config: dict):
@@ -13,3 +15,4 @@ class A:
 
 
 assert A(a='no', config={'a': 'hi', 'b': 1, 'config': {'a': 'ohmy'}}) == A(a='ohmy', b=0)
+assert A(a='no', lst=[1, 2, 3], config={'a': 'hi', 'b': 1}) == A(a='hi', b=1, lst=[])
